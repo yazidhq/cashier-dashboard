997cce4abba9ed7ca56f0a5616d546b77658c78e
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-06-01 20:11:47 +0700

    test min width

--- src/components/product/ProductsCard.jsx
+++ src/components/product/ProductsCard.jsx
@@ -3,10 +3,13 @@ import TitleMenu from "../TitleMenu";
 import { useState } from "react";
 import Category from "../category/Category";
 import { IoFilterOutline } from "react-icons/io5";
+import { MdWidthFull } from "react-icons/md";
 
 const Products = ({ handleAddOrder, showCategory, handleCategory }) => {
   const product = JSON.parse(localStorage.getItem("products"));
   const [products] = useState(product ? product : []);
+
+  const isProduct = JSON.parse(localStorage.getItem("products"));
 
   const renderProducts = products.map((item) => {
     return showCategory === "all" || item.category == showCategory ? (
@@ -15,14 +18,15 @@ const Products = ({ handleAddOrder, showCategory, handleCategory }) => {
         item={item}
         handleAddOrder={handleAddOrder}
       />
-    ) : (
-      <div key={item.name} style={{ paddingInline: "150px" }}></div>
-    );
+    ) : null;
   });
 
   return (
     <div style={{ paddingLeft: "2.5rem" }}>
-      <div className="flex-grow-1 px-5 pt-4 bg-light pb-1">
+      <div
+        className="flex-grow-1 px-5 pt-4 bg-light pb-1"
+        style={{ minWidth: "700px" }}
+      >
         <Category handleCategory={handleCategory} />
         <div className="mb-5">
           <div className="d-flex justify-content-between">
