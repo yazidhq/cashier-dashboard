88d6d21c60d94be2a72672d5d99e45457a0550bc
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2024-06-01 20:13:48 +0700

    test column card

--- src/components/product/ProductsCard.jsx
+++ src/components/product/ProductsCard.jsx
@@ -38,7 +38,7 @@ const Products = ({ handleAddOrder, showCategory, handleCategory }) => {
               <p onClick={() => handleCategory("all")}>show all menu</p>
             </div>
           </div>
-          <div className="row row-cols-1 row-cols-lg-3 g-4">
+          <div className="row row-cols-1 row-cols-md-3 g-4">
             {renderProducts}
           </div>
         </div>
